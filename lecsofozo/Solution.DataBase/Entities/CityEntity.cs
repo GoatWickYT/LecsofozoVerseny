37ba39b68fa1864c7302e0395d974c904595f66f
Wittner Attila <b1witatt@vasvari.org>
2025-03-19 09:39:26 +0100

    database set

--- lecsofozo/Solution.DataBase/Entities/CityEntity.cs
+++ lecsofozo/Solution.DataBase/Entities/CityEntity.cs
@@ -4,6 +4,10 @@
 public class CityEntity
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public uint Id { get; set; }
+
+    [Required]
     public uint PostalCode { get; set; }
 
     [Required]
--- lecsofozo/Solution.DataBase/Entities/TeamEntity.cs
+++ lecsofozo/Solution.DataBase/Entities/TeamEntity.cs
@@ -5,7 +5,7 @@ public class TeamEntity
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    public uint Id { get; set; }
 
     [Required]
     public string Name { get; set; }
