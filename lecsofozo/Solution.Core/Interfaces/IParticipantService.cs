3635174c3f3c3655695e7d8ef95a23da46315155
Wittner Attila <b1witatt@vasvari.org>
2025-03-27 08:18:39 +0100

    Basic interfaces added

---
+++ lecsofozo/Solution.Core/Interfaces/IParticipantService.cs
@@ -0,0 +1,12 @@
+﻿namespace Solution.Core.Interfaces;
+
+public interface IParticipantService
+{
+    Task<ErrorOr<ParticipantModel>> CreateAsync(ParticipantModel model);
+    Task<ErrorOr<Success>> UpdateAsync(ParticipantModel model);
+    Task<ErrorOr<Success>> DeleteAsync(string id);
+    Task<ErrorOr<ParticipantModel>> GetByIdAsync(string id);
+    Task<ErrorOr<List<ParticipantModel>>> GetAllAsync();
+    Task<ErrorOr<List<ParticipantModel>>> GetPagedAsync(int page = 0);
+    Task<int> GetMaxPageNumberAsync();
+}
