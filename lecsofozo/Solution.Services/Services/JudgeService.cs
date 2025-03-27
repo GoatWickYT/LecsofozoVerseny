d4e503277dcc77fb09742b402506b0bc09ad1cfe
Wittner Attila <b1witatt@vasvari.org>
2025-03-27 09:18:05 +0100

    theoratically done with services tweaking may be required

---
+++ lecsofozo/Solution.Services/Services/JudgeService.cs
@@ -0,0 +1,81 @@
+﻿namespace Solution.Services.Services;
+
+public class JudgeService(AppDbContext dbContext) : IJudgeService
+{
+    private int ROW_COUNT = 5;
+
+    public async Task<ErrorOr<JudgeModel>> CreateAsync(JudgeModel model)
+    {
+        bool exists = dbContext.Judges.Any(j => j.Name.ToLower() == model.Name.Value.ToLower() &&
+                                                j.PhoneNumber.ToLower() == model.PhoneNumber.Value.ToLower() &&
+                                                j.Email.ToLower() == model.Email.Value.ToLower());
+
+        if (exists)
+        {
+            return Error.Conflict(description: "Judge already exists.");
+        }
+
+        var entity = model.ToEntity();
+
+        entity.PublicId = Guid.NewGuid().ToString();
+
+        await dbContext.Judges.AddAsync(entity);
+        await dbContext.SaveChangesAsync();
+
+        return new JudgeModel(entity);
+    }
+
+    public async Task<ErrorOr<Success>> DeleteAsync(string id)
+    {
+        var result = await dbContext.Judges.AsNoTracking()
+                                           .Where(x => x.PublicId == id)
+                                           .ExecuteDeleteAsync();
+
+        return result > 0 ? Result.Success : Error.NotFound();
+    }
+
+    public async Task<ErrorOr<List<JudgeModel>>> GetAllAsync() =>
+        await dbContext.Judges.AsNoTracking()
+                              .Select(j => new JudgeModel(j))
+                              .ToListAsync();
+
+    public async Task<ErrorOr<JudgeModel>> GetByIdAsync(string id)
+    {
+        var entity = await dbContext.Judges.AsNoTracking()
+                                          .FirstOrDefaultAsync(j => j.PublicId == id);
+
+        return entity is null ? Error.NotFound() : new JudgeModel(entity);
+    }
+
+    public Task<int> GetMaxPageNumberAsync()
+    {
+        return dbContext.Judges.AsNoTracking()
+                               .CountAsync()
+                               .ContinueWith(t => (int)Math.Ceiling(t.Result / (double)ROW_COUNT));
+    }
+
+    public async Task<ErrorOr<List<JudgeModel>>> GetPagedAsync(int page = 0)
+    {
+        page = page < 0 ? 0 : page - 1;
+
+        return await dbContext.Judges.AsNoTracking()
+                           .Skip(page * ROW_COUNT)
+                           .Take(ROW_COUNT)
+                           .Select(x => new JudgeModel(x))
+                           .ToListAsync();
+    }
+
+    public async Task<ErrorOr<Success>> UpdateAsync(JudgeModel model)
+    {
+        var result = await dbContext.Judges.AsNoTracking()
+                                           .Where(x => x.PublicId == model.PublicId)
+                                           .ExecuteUpdateAsync(x => x.SetProperty(p => p.PublicId, model.PublicId)
+                                                                     .SetProperty(p => p.Name, model.Name.Value)
+                                                                     .SetProperty(p => p.Email, model.Email.Value)
+                                                                     .SetProperty(p => p.PhoneNumber, model.PhoneNumber.Value)
+                                                                     .SetProperty(p => p.WebContentLink, model.WebContentLink)
+                                                                     .SetProperty(p => p.ImageId, model.ImageId));
+
+        return result > 0 ? Result.Success : Error.NotFound();
+    }
+}
---
+++ lecsofozo/Solution.Services/Services/TeamService.cs
@@ -0,0 +1,77 @@
+﻿namespace Solution.Services.Services;
+
+public class ParticipantService(AppDbContext dbContext) : IParticipantService
+{
+    private int ROW_COUNT = 5;
+
+    public async Task<ErrorOr<ParticipantModel>> CreateAsync(ParticipantModel model)
+    {
+        bool exists = dbContext.Participants.Any(p => p.Name.ToLower() == model.Name.Value.ToLower());
+
+        if (exists)
+        {
+            return Error.Conflict(description: "Participant already exists.");
+        }
+
+        var entity = model.ToEntity();
+
+        entity.PublicId = Guid.NewGuid().ToString();
+
+        await dbContext.Participants.AddAsync(entity);
+        await dbContext.SaveChangesAsync();
+
+        return new ParticipantModel(entity);
+    }
+
+    public async Task<ErrorOr<Success>> DeleteAsync(string id)
+    {
+        var result = await dbContext.Participants.AsNoTracking()
+                                           .Where(p => p.PublicId == id)
+                                           .ExecuteDeleteAsync();
+
+        return result > 0 ? Result.Success : Error.NotFound();
+    }
+
+    public async Task<ErrorOr<List<ParticipantModel>>> GetAllAsync() =>
+        await dbContext.Participants.AsNoTracking()
+                              .Select(p => new ParticipantModel(p))
+                              .ToListAsync();
+
+    public async Task<ErrorOr<ParticipantModel>> GetByIdAsync(string id)
+    {
+        var entity = await dbContext.Participants.AsNoTracking()
+                                          .FirstOrDefaultAsync(p => p.PublicId == id);
+
+        return entity is null ? Error.NotFound() : new ParticipantModel(entity);
+    }
+
+    public Task<int> GetMaxPageNumberAsync()
+    {
+        return dbContext.Participants.AsNoTracking()
+                                     .CountAsync()
+                                     .ContinueWith(t => (int)Math.Ceiling(t.Result / (double)ROW_COUNT));
+    }
+
+    public async Task<ErrorOr<List<ParticipantModel>>> GetPagedAsync(int page = 0)
+    {
+        page = page < 0 ? 0 : page - 1;
+
+        return await dbContext.Participants.AsNoTracking()
+                                           .Skip(page * ROW_COUNT)
+                                           .Take(ROW_COUNT)
+                                           .Select(x => new ParticipantModel(x))
+                                           .ToListAsync();
+    }
+
+    public async Task<ErrorOr<Success>> UpdateAsync(ParticipantModel model)
+    {
+        var result = await dbContext.Participants.AsNoTracking()
+                                                 .Where(x => x.PublicId == model.PublicId)
+                                                 .ExecuteUpdateAsync(x => x.SetProperty(p => p.PublicId, model.PublicId)
+                                                                           .SetProperty(p => p.Name, model.Name.Value)
+                                                                           .SetProperty(p => p.WebContentLink, model.WebContentLink)
+                                                                           .SetProperty(p => p.ImageId, model.ImageId));
+
+        return result > 0 ? Result.Success : Error.NotFound();
+    }
+}
